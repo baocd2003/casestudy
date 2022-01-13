6120d4d6b75d37ecd7e58fbdc95c7e7c5ebb6218
baocd2003 <BaoCao494@gmail.com>
2022-01-13 20:39:06 +0700

    update

---
+++ game.js
@@ -0,0 +1,57 @@
+let dino = document.querySelector('.dino');
+let obstacle = document.querySelector('.obstacle');
+let score = document.querySelector('.score');
+let ground = document.querySelector('.ground');
+let playerScore = 0;
+let scoreRun;
+let scoreCount = function(){
+	playerScore ++;
+	score.innerHTML = `Score: <b>${playerScore}</b>`;
+	if(playerScore == 50){
+		document.querySelector('.container').classList.add('container-1');
+		obstacle.classList.add('obs-1')
+	}
+}
+window.addEventListener("keydown",function(key){
+	if(key.code == 'Enter'){
+		ground.classList.add("groundRun");
+		obstacle.classList.add("obs");
+		scoreRun = setInterval(scoreCount,300);	
+		document.querySelector('.lose').style.display = 'none';
+	}
+})
+window.addEventListener("keydown",function(play){
+	if(play.code == "Space"){		
+		if(dino.classList != "dinoJump"){
+			dino.classList.add("dinoJump");
+			setTimeout(function(){
+				dino.classList.remove("dinoJump");
+			},450);
+			
+		}
+	}
+})
+
+let lose  = setInterval(function(){
+	let bot = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
+	// console.log(bot)
+	let left = parseInt(getComputedStyle(obstacle).getPropertyValue("left"));
+	// console.log(left)
+	if( bot <= 90  && left <= 65 && left >= 20){
+		console.log("Game over");
+		obstacle.classList.remove("obs");
+		document.querySelector('.container').classList.remove('container-1');
+		obstacle.classList.remove('obs-1')
+		ground.classList.remove("groundRun");
+		document.querySelector('.lose').style.display = 'block';
+		let arr = [];
+		let output = arr.push(playerScore);
+		console.log(output);
+		clearInterval(scoreRun);
+		playerScore = 00;
+		score.innerHTML = `Score : <b>${playerScore}</b>`;
+		
+		return;
+	}
+},20)
+
